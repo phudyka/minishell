a2013e07d068888e01bcc14be6438004e31bf97a
phudyka <paul.hudyka@gmail.com>
2023-08-04 21:50:29 +0200

    signals 80% ok

--- src/main/signals.c
+++ src/main/signals.c
@@ -6,7 +6,7 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:44:16 by phudyka           #+#    #+#             */
-/*   Updated: 2023/08/04 16:54:11 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/08/04 21:15:17 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,25 @@ static void	ft_sigint(int sig)
 static void	ft_sigterm(int sig)
 {
 	(void)sig;
+	if (g_shell.data->pid > 0)
+		kill(g_shell.data->pid , SIGTERM);
 	exit(EXIT_SUCCESS);
 }
 
 static void	ft_sigquit(int sig)
 {
 	(void)sig;
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	rl_redisplay();
+	  if (g_shell.data->pid > 0)
+	{
+		kill(g_shell.data->pid, SIGINT);
+		ft_putstr_fd("\n", 1);
+	}
+	else
+	{
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
 }
 
 void	ft_signals(void)
