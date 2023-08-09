f832f6230f1bdbf1fa7cc53870a733fc1f6aaf8e
David TASSEL <davidtassel04@gmail.com>
2023-08-09 08:10:38 -0400

    fix again dollar

--- src/expander/dollar.c
+++ src/expander/dollar.c
@@ -6,7 +6,7 @@
 /*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 16:14:16 by phudyka           #+#    #+#             */
-/*   Updated: 2023/08/09 07:30:17 by kali             ###   ########.fr       */
+/*   Updated: 2023/08/09 08:05:47 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,31 @@ static char	*handle_question_mark(const char *str, size_t *i, char *output, size
 	return (NULL);
 }
 
+static char	*handle_dollar_and_increment(const char *str, size_t *i, char *output, size_t *j)
+{
+	char	var_name[256];
+	size_t	k;
+	char	*env_value;
+
+	ft_bzero(var_name, 256);
+	k = 0;
+	if (isalnum(str[*i + 1]) || str[*i + 1] == '_')
+	{
+		(*i)++;
+		while (isalnum(str[*i]) || str[*i] == '_')
+			var_name[k++] = str[(*i)++];
+		env_value = get_from_env(var_name, g_shell.env);
+		if (env_value)
+		{
+			ft_strcpy(output + *j, env_value);
+			*j += ft_strlen(env_value);
+		}
+	}
+	else
+		output[(*j)++] = str[(*i)++];
+	return (output);
+}
+
 char	*ft_dollar(const char *str, int sqot)
 {
 	size_t	output_size;
@@ -77,8 +102,7 @@ char	*ft_dollar(const char *str, int sqot)
 	size_t	j;
 
 	output_size = calc_output_size(str);
-	output = (char *)malloc(output_size + 1);
-	if (!output)
+	if (!(output = (char *)malloc(output_size + 1)))
 		return (NULL);
 	i = 0;
 	j = 0;
@@ -87,26 +111,8 @@ char	*ft_dollar(const char *str, int sqot)
 		if (str[i] == '$' && !sqot)
 		{
 			if (handle_question_mark(str, &i, output, &j))
-				continue ;
-			if (isalnum(str[i + 1]) || str[i + 1] == '_')
-			{
-				i++;
-				char var_name[256] = {0};
-				size_t k = 0;
-				while (isalnum(str[i]) || str[i] == '_')
-					var_name[k++] = str[i++];
-				char *env_value = getenv(var_name);
-				if (env_value)
-				{
-					ft_strcpy(output + j, env_value);
-					j += ft_strlen(env_value);
-				}
-				else
-				{
-					ft_strcpy(output + j, var_name);
-					j += k;
-				}
-			}
+				continue;
+			output = handle_dollar_and_increment(str, &i, output, &j);
 		}
 		else
 			output[j++] = str[i++];
