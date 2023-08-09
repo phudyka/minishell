1181f16330e24c4511d4b6a5cb780a2e4a4b5475
David TASSEL <davidtassel04@gmail.com>
2023-08-09 04:21:05 -0400

    fix dollar and split

--- src/expander/dollar.c
+++ src/expander/dollar.c
@@ -3,82 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   dollar.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 16:14:16 by phudyka           #+#    #+#             */
-/*   Updated: 2023/08/08 19:58:09 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/08/09 04:06:05 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../include/main.h"
+#include "../../include/main.h"
 
 char *ft_strcpy(char *dest, const char *src)
 {
-    size_t i;
-
-    i = -1;
-    while (src[++i])
+    size_t i = 0;
+    while (src[i])
+    {
         dest[i] = src[i];
+        i++;
+    }
     dest[i] = '\0';
     return (dest);
 }
 
 char *command_status(const char *input)
 {
-    char *output;
-    
-    output = (char *)malloc(ft_strlen(input) * 2);
+    char *output = (char *)malloc(ft_strlen(input) * 2 + 10); // Added some extra space for the integer
     if (!output)
     {
         perror("malloc");
         return (NULL);
     }
-    printf("%s%d", input, g_shell.status);
+    snprintf(output, ft_strlen(input) * 2 + 10, "%s%d", input, g_shell.status);
     return (output);
 }
 
 char *ft_dollar(const char *str, int sqot)
 {
-    size_t i;
-    size_t j;
-    char *output;
-    char *env_value;
+    size_t i = 0, j = 0;
+    char *output = (char *)malloc(ft_strlen(str) + 1);
 
-    i = 0;
-    j = 0;
-    output = (char *)malloc(ft_strlen(str) + 1);
     if (!output)
     {
         perror("malloc");
         return (NULL);
     }
+
     while (str[i])
     {
-        if (str[i] == '$' && !sqot)
+        if (str[i] == '$' && !sqot && (isalnum(str[i+1]) || str[i+1] == '_'))
         {
             i++;
-            char var_name[256];
+            char var_name[256] = {0};
             size_t k = 0;
             while (isalnum(str[i]) || str[i] == '_')
                 var_name[k++] = str[i++];
-            var_name[k] = '\0';
-            env_value = getenv(var_name);
+            char *env_value = getenv(var_name);
             if (env_value)
             {
-                size_t env_len = strlen(env_value);
-                if (j + env_len >= ft_strlen(str) + 1)
+                output = realloc(output, j + strlen(env_value) + ft_strlen(str) - i + 1);
+                if (!output)
                 {
-                    perror("Output buffer overflow");
-                    free(output);
+                    perror("realloc");
                     return (NULL);
                 }
                 strcpy(output + j, env_value);
-                j += env_len;
+                j += strlen(env_value);
             }
         }
         else
+        {
             output[j++] = str[i++];
+        }
     }
     output[j] = '\0';
     return (output);
 }
+
--- src/parser/split.c
+++ src/parser/split.c
@@ -3,32 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 18:47:20 by phudyka           #+#    #+#             */
-/*   Updated: 2023/08/08 20:00:53 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/08/09 04:08:43 by kali             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../include/parser.h"
+#include "../../include/parser.h"
 
 static int is_valid_quote(const char *s)
 {
     char quote_char = 0;
-
     while (*s)
     {
         if (quote_char == 0 && (*s == '\'' || *s == '"'))
-        {
             quote_char = *s;
-        }
         else if (*s == quote_char)
-        {
             quote_char = 0;
-        }
         s++;
     }
-    return (quote_char == 0);  // Retourne 0 si un guillemet n'est pas fermé
+    return (quote_char == 0);
 }
 
 static const char *next_word_start(const char *s)
@@ -65,10 +60,8 @@ static unsigned int count_words_buff(const char *str)
     {
         str = next_word_start(str);
         quote_char = (*str == '\'' || *str == '"') ? *str : 0;
-
         if (quote_char)
             str++;
-
         str = next_word_end(str, quote_char);
         if (quote_char && *str == quote_char)
             str++;
@@ -82,21 +75,20 @@ char **ft_split_buff(char const *s)
     char        **strs;
     size_t      i;
     const char  *start, *end;
-    int         sqot;
+    int         sqot = 0;
     char        quote_char;
 
-    if (!s)
-        return (NULL);
-    if (!is_valid_quote(s))
+    if (!s || !is_valid_quote(s))
     {
         ft_putendl_fd("Error ! [Open Quotes]", STDERR_FILENO);
         return (NULL);
     }
-    i = 0;
+
     strs = (char **)malloc(sizeof(char *) * (count_words_buff(s) + 1));
     if (!strs)
         return (NULL);
-        
+
+    i = 0;
     while (*s)
     {
         s = next_word_start(s);
@@ -106,12 +98,18 @@ char **ft_split_buff(char const *s)
         end = s;
         if (quote_char == '\'')
             sqot = 1;
+        else
+            sqot = 0;
         if (start < end)
         {
-            if (quote_char && start < end - 1 && *(end - 1) == quote_char)
-                strs[i++] = ft_dollar(ft_substr(start, 0, end - start - 1), sqot);
-            else
-                strs[i++] = ft_dollar(ft_substr(start, 0, end - start), sqot);
+            char *substr_result = (quote_char && start < end - 1 && *(end - 1) == quote_char) ? ft_substr(start, 0, end - start - 1) : ft_substr(start, 0, end - start);
+            if (!substr_result)
+            {
+                free(strs); // Free the memory allocated for strs
+                return NULL;
+            }
+            strs[i++] = ft_dollar(substr_result, sqot);
+            free(substr_result);
         }
     }
     strs[i] = NULL;
