64a4c5835bc7ed16a21a906aa99ae465c1040605
phudyka <paul.hudyka@gmail.com>
2023-08-10 20:02:46 +0200

    signals mucho perfect

--- src/main/signals.c
+++ src/main/signals.c
@@ -6,11 +6,27 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 13:44:16 by phudyka           #+#    #+#             */
-/*   Updated: 2023/08/10 17:44:48 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/08/10 19:55:04 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/main.h"
+
+static void	ft_sigquit(int sig)
+{
+	(void)sig;
+	if (g_shell.pid > 0)
+	{
+		kill(g_shell.pid, SIGQUIT);
+		ft_putstr_fd("^\\Quit (core dumped)", 2);
+	}
+	else
+	{
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
 
 void	restore_termios(void)
 {
@@ -47,6 +63,8 @@ static void	ft_sigterm(int sig)
 
 void	ft_signals(void)
 {
+	struct termios	new_termios;
+
 	g_shell.termios = (struct termios *)malloc(sizeof(struct termios));
 	if (tcgetattr(STDIN_FILENO, g_shell.termios) == -1)
 	{
@@ -54,7 +72,15 @@ void	ft_signals(void)
 		g_shell.status = 2;
 		exit(EXIT_FAILURE);
 	}
+	new_termios = *g_shell.termios;
+	new_termios.c_lflag &= ~ECHOCTL;
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &new_termios) == -1)
+	{
+		ft_putstr_fd("Error! [tcsetattr]\n", 2);
+		g_shell.status = 2;
+		exit(EXIT_FAILURE);
+	}
 	signal(SIGINT, ft_sigint);
 	signal(SIGTERM, ft_sigterm);
-	signal(SIGQUIT, SIG_IGN);
+	signal(SIGQUIT, ft_sigquit);
 }
