99b02dd2dfeb6c2e84000373aa95e8a2c552cb6e
phudyka <paul.hudyka@gmail.com>
2023-08-10 18:43:16 +0200

    QUOTES OK TA RACE MAUDITE

--- src/parser/quotes.c
+++ src/parser/quotes.c
@@ -6,80 +6,66 @@
 /*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 18:24:06 by phudyka           #+#    #+#             */
-/*   Updated: 2023/08/10 18:24:09 by phudyka          ###   ########.fr       */
+/*   Updated: 2023/08/10 18:39:51 by phudyka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/parser.h"
 
-static void	ft_dquote(char *parsed, int *j, const char *value)
+static void	ft_dquote(int *i, int *j, char *parsed, const char *value)
 {
-	int		i;
 	char	*env_var;
 
-	i = 0;
-	while (value[i])
+	while (value[(*i)])
 	{
-		if (value[i] == '$')
+		if (value[(*i)] == '$')
 		{
-			i++;
-			env_var = ft_dollar(value + i, 0);
+			(*i)++;
+			env_var = ft_dollar(value + (*i), 0);
 			if (env_var)
 			{
 				ft_strcpy(parsed + *j, env_var);
 				*j += ft_strlen(env_var);
 				free(env_var);
 			}
-			while (value[i] && value[i] != '"')
-				parsed[(*j)++] = value[i++];
+			while (value[(*i)] && value[(*i)] != '"')
+				parsed[(*j)++] = value[(*i)++];
 		}
 		else
-			parsed[(*j)++] = value[i++];
+			parsed[(*j)++] = value[(*i)++];
 	}
 }
 
-static void	ft_squote(char *parsed, int *j, const char *value)
+static void	ft_squote(int *i, int *j, char *parsed, const char *value)
 {
-	int	i;
-
-	i = 0;
-	while (value[i])
-		parsed[(*j)++] = value[i++];
+	while (value[(*i)])
+		parsed[(*j)++] = value[(*i)++];
 }
 
-static void	ft_sequence(int *i, int len, char *parsed, const char *value)
+static void	ft_sequence(int len, char *parsed, const char *value)
 {
+	int	i;
 	int	j;
 
+	i = 0;
 	j = 0;
-	(*i)++;
-	while (*i < len && value[*i])
+	while (i < len && value[i])
 	{
-		if (value[*i] == '"')
-		{
-			ft_dquote(parsed, &j, value + ++(*i));
-			if (value[*i] == '"')
-				(*i)++;
-		}
-		else if (value[*i] == '\'')
-		{
-			ft_squote(parsed, &j, value + ++(*i));
-			if (value[*i] == '\'')
-				(*i)++;
-		}
+		if (value[i] == '"')
+			ft_dquote(&i, &j, parsed, value + i);
+		else if (value[i] == '\'')
+			ft_squote(&i, &j, parsed, value + i);
 		else
-			parsed[(j)++] = value[(*i)++];
+			parsed[j++] = value[i++];
 	}
 	parsed[j] = '\0';
 }
 
 void	parse_quotes(t_token *tokens)
 {
-	int		i;
 	int		len;
 	char	*parsed;
 
-	i = 0;
 	len = ft_strlen(tokens->value);
 	parsed = (char *)malloc(sizeof(char) * (len + 1));
 	if (!parsed)
@@ -88,7 +74,7 @@ void	parse_quotes(t_token *tokens)
 		g_shell.status = 12;
 		return ;
 	}
-	ft_sequence(&i, len, parsed, tokens->value);
+	ft_sequence(len, parsed, tokens->value);
 	free(tokens->value);
 	tokens->value = parsed;
 }
